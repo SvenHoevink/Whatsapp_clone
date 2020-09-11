17b320383f14de35dc7e4f83ed672216c61e9099
SvenHoevink <svenhoevink@gmail.com>
2020-09-12 00:23:27 +0200

    added chat body + footer

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -4,6 +4,8 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { Avatar, IconButton } from '@material-ui/core';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import SearchIcon from '@material-ui/icons/Search';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
 
 function Chat() {
 	return (
@@ -19,12 +21,37 @@ function Chat() {
 						<SearchIcon />
 					</IconButton>
 					<IconButton>
-						<AttachFileIcon />
-					</IconButton>
-					<IconButton>
 						<MoreVertIcon />
 					</IconButton>
 				</div>
+			</div>
+			<div className="chat_body">
+				<p className="chat_message">
+					<span className="chat_name">Sven</span>
+					This is a message
+					<span className="chat_timestamp">{new Date().toUTCString()}</span>
+				</p>
+
+				<p className="chat_receiver chat_message">
+					<span className="chat_name">Frank</span>
+					This is a message
+					<span className="chat_timestamp">{new Date().toUTCString()}</span>
+				</p>
+			</div>
+			<div className="chat_footer">
+				<IconButton>
+					<InsertEmoticonIcon />
+				</IconButton>
+				<IconButton>
+					<AttachFileIcon />
+				</IconButton>
+				<form className="chat_form">
+					<input type="text" className="chat_text" />
+					<input type="submit" className="chat_button" placeholder="Send a message" />
+				</form>
+				<IconButton>
+					<MicIcon />
+				</IconButton>
 			</div>
 		</div>
 	);
