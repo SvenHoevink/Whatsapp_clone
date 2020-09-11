c8aeea15a053a003408782620b359ba5ed73785f
SvenHoevink <svenhoevink@gmail.com>
2020-09-11 20:56:01 +0200

    added chat description

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './Chat.css';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Avatar, IconButton } from '@material-ui/core';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Chat() {
 	return (
-		<div>
-			<h1>I am the chat</h1>
+		<div className="chat">
+			<div className="chat_header">
+				<Avatar />
+				<div className="chat_headerInfo">
+					<h2>Room name</h2>
+					<p>Last seen online: </p>
+				</div>
+				<div className="chat_headerRight">
+					<IconButton>
+						<SearchIcon />
+					</IconButton>
+					<IconButton>
+						<AttachFileIcon />
+					</IconButton>
+					<IconButton>
+						<MoreVertIcon />
+					</IconButton>
+				</div>
+			</div>
 		</div>
 	);
 }
